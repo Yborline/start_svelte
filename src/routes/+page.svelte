f9fa5c34274c8412ee5494e687bbf4cbbbbb1a57
Yaroslav Borys <borrline@gmail.com>
2023-09-28 21:22:36 +0300

    attemptstart

---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+  const people = [
+    { id: 1, name: "Maksim" },
+    { id: 2, name: "Elena" },
+  ];
+</script>
+
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+
+<!-- <script>
+  import Person from "./Person.svelte";
+
+  const people = [
+    { id: 1, name: "Maxim", age: 24, job: "Frontend" },
+    { id: 2, name: "Elena", age: 17, job: "Student" },
+  ];
+
+  let name = "Igor";
+  let age = 20;
+  let jobTitle = "Backend";
+</>
+
+<h1>Application</h1>
+<Person name={people[0].name} age={people[0].age} job={people[0].job} />
+<Person {...people[1]} />
+<Person {name} {age} job={jobTitle} />
+<style>
+</> -->
+
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+
+<!-- <script>
+  //   import Main from "./Main.svelte";
+
+  let value = "Hello";
+  $: error = !isValid(value);
+
+  function isValid(val) {
+    return val.length > 5 && val.length < 10;
+  }
+</script>
+
+<h1>Application {value}</h1>
+
+<input type="text" bind:value class:red={error} class:green={!error} />
+{#if value.length < 5}
+  <p>Длина меньше 5 символов</p>
+{:else if value.length > 10}
+  <p>Длина больше 10 символов</p>
+{:else}
+  <p>Длина между 5 и 10 символов</p>
+{/if}
+
+<style>
+  input {
+    outline: none;
+  }
+
+  .red {
+    border-color: red;
+  }
+  .green {
+    border-color: green;
+  }
+</style> -->
+
+<style>
+</style>
---
+++ src/routes/Person.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name = "Undefined name";
+  export let age = "Undefined age";
+  export let job = "Undefined job";
+</script>
+
+<div>
+  <p>Имя: <b>{name}</b></p>
+  <p>Занятие: <b>{job}</b></p>
+
+  <p>age: <b>{age}</b></p>
+  {#if age < 18}
+    <p>Пиво продавать нельзя</p>
+  {:else}
+    <p>Делай что хочешь</p>
+  {/if}
+</div>
+
+<style>
+  div {
+    border: 1px solid black;
+    padding: 1rem;
+    margin-bottom: 1rem;
+  }
+</style>
